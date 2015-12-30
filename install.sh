714b4fb9c7286ea9879d1774afa49e328cebb90b
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2015-12-30 22:15:19 +0100

    Update installation script with comments

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#This script will overwrite your current .vimrc wit the one of the repo
+#Takes care of installing the most neccesiry dependencies
 cp .vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
