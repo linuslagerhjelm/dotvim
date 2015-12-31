47ad78e36a21382543eda49152f19d8942be09ce
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2015-12-31 10:47:14 +0100

    Installscript will now update your vim version as well

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #This script will overwrite your current .vimrc wit the one of the repo
 #Takes care of installing the most neccesiry dependencies
+brew install macvim --with-override-system-vim
 cp .vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
